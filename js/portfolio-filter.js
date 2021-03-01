0f969e95ac81d753a56abd617c4261164cd4906e
Vilayphone Vilaysouk <vilayphone.vilaysouk@umontreal.ca>
2021-02-28 19:13:02 -0500

    portfolio

---
+++ js/portfolio-filter.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//Filter project cards
+var previousClickedMenuLink = undefined;
+$('.portfolio-menu').on('click', 'a', function(event){
+    event.preventDefault();
+
+    if (previousClickedMenuLink) {
+        previousClickedMenuLink.removeClass('portfolio-menu__link--active');
+    }
+    var link = $(event.target);
+    link.addClass('portfolio-menu__link--active');
+    previousClickedMenuLink = link;
+
+    var targetTag = $(event.target).data('portfolio-target-tag');
+    var targetTag2 = $(event.target).data('portfolio-target-tag2');
+    var portfolioItems = $('.portfolio-cards').children();
+
+    if (targetTag === 'all') {
+        portfolioItems.fadeIn({duration: 500});
+    } else {
+        portfolioItems.hide();
+    }
+
+    portfolioItems.each(function(index, value){
+        var item = $(value);
+        if (item.data('portfolio-tag') === targetTag) {
+            item.fadeIn({duration: 500});
+        }
+        if (item.data('portfolio-tag2') === targetTag) {
+            item.fadeIn({duration: 500});
+        }
+    });
+});
